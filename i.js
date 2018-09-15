797ce22be1460e16f1a7fc61deea0b96523c7913
sKhcodes <sarahhkhazma@gmail.com>
2018-09-15 10:50:24 +0100

    returned script to external file

--- i.js
+++ i.js
@@ -1,11 +1,10 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+let i = 0;
+let txt = 'Lorem ipsum typing effect!';
+let speed = 100; 
 window.onload = function typeWriter() {
     if (i < txt.length) {
-        document.querySelector("firstOne").innerHTML += txt.charAt(i);
+        document.querySelector(".firstOne").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
 }
-
