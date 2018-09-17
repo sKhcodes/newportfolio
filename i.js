5bac4b90d8bf61f11d08ca14a2961e91ca50c862
sKhcodes <sarahhkhazma@gmail.com>
2018-09-17 09:49:03 +0100

    made cursor flash

--- i.js
+++ i.js
@@ -3,8 +3,8 @@ let txt = 'Lorem ipsum typing effect!';
 let speed = 100; 
 window.onload = function typeWriter() {
     if (i < txt.length) {
-        document.querySelector(".firstOne").innerHTML += txt.charAt(i);
-        i++;
+document.querySelector(".firstOne").innerHTML += txt.charAt(i);
+       i++;
         setTimeout(typeWriter, speed);
     }
 }
