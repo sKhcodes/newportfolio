46cf95eae44eabfab7cd3d228477c7b4796e0dda
sKhcodes <sarahhkhazma@gmail.com>
2018-09-28 18:01:13 +0100

    animated button

--- i.js
+++ i.js
@@ -6,7 +6,7 @@ let speed = 100;
 let firstdiv = document.querySelector("#firstOne");
 let nextdiv = document.querySelector("#nextOne");
 let lastdiv = document.querySelector("#lastOne");
-
+let btn = document.querySelector("button");
 function typeWriter(txtname, divname, i) {
     i || (i = 0);
 
@@ -19,3 +19,12 @@ function typeWriter(txtname, divname, i) {
 window.onload = function() { typeWriter(txtOne, firstdiv) };
 firstdiv.addEventListener("animationend", function() { typeWriter(txtTwo, nextdiv) });
 nextdiv.addEventListener("animationend", function() { typeWriter(txtThree, lastdiv) });
+lastdiv.addEventListener("animationend", function() { 
+   btn.style.display = "block";
+   btn.style.animation = "animate-drop 1s linear";
+ });
+
+ btn.addEventListener("click", function() {
+     document.querySelector("#about").style.display = "block";
+ })
+ 
