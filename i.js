98018dad4252a76f3a086395c6424bbbd27abf81
sKhcodes <sarahhkhazma@gmail.com>
2018-10-08 11:53:33 +0100

    Added JS for flexpanels first part

--- i.js
+++ i.js
@@ -1,3 +1,5 @@
+// Typewriter section
+
 let i = 0;
 let txtOne = 'Hi.';
 let txtTwo = 'My name is Sarah ';
@@ -18,16 +20,25 @@ function typeWriter(txtname, divname, i) {
 }
 
 window.onload = function() { typeWriter(txtOne, firstdiv) };
+
+// Timing animations and typewriter
 firstdiv.addEventListener("animationend", function() { typeWriter(txtTwo, nextdiv) });
 nextdiv.addEventListener("animationend", function() { typeWriter(txtThree, lastdiv) });
 lastdiv.addEventListener("animationend", function() { 
    btn.style.display = "block";
    btn.style.animation = "animate-drop 1s linear";
  });
-
+//   Button to reveal rest of portfolio
  btn.addEventListener("click", function() {
      for (div of sect) {
          div.style.display = "inline-grid";
      }
  })
+
+//  Flex panels for projects
+const projects= document.querySelectorAll('.project');
+projects.forEach(project => projects.addEventListener('click', () => {
+  project.classList.toggle('open');
+}));
+
  
